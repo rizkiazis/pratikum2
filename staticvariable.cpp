d3fabc21f737ac64433534872c1520f4a69348b2
rizkiazis <rizkiajismaulana@gmail.com>
2018-12-02 08:21:14 +0700

    masukan data baru

---
+++ staticvariable.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void hitung() {
+     int total =0;
+    total++;
+    cout << "Nilai Total = " << total << endl;
+}
+
+int main(){
+    hitung();
+    hitung();
+    hitung();
+}
